b8c6a39f032718a4163fabb0ddcfc191c5d370a6
Facwl <2864235156@qq.com>
2021-11-03 04:27:34 +0800

    bengle

--- thrift-server/src/main/java/benchmark/rpc/Server.java
+++ thrift-server/src/main/java/benchmark/rpc/Server.java
@@ -14,24 +14,29 @@ import java.net.InetSocketAddress;
 import java.sql.Timestamp;
 
 public class Server {
-	public static void main(String[] args) throws TTransportException {
-		System.out.println("in thrift Server main() ");
-		InetSocketAddress serverAddress = new InetSocketAddress("benchmark-server", 8080);
-		System.out.println("in thrift Server main() ==2==");
-		TNonblockingServerTransport serverSocket = new TNonblockingServerSocket(serverAddress);
+	public static void main(String[] args) {
+		try {
 
-		TThreadedSelectorServer.Args serverParams = new TThreadedSelectorServer.Args(serverSocket);
-		System.out.println("in thrift Server main() ==3==");
-		serverParams.protocolFactory(new TBinaryProtocol.Factory());
-		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-		System.out.println("in thrift Server main() ==4=="+timestamp.toString());
-		serverParams.processor(new UserService.Processor<UserService.Iface>(new UserServiceServerImpl()));
-		System.out.println("in thrift Server main() ==5==");
-		TServer server = new TThreadedSelectorServer(serverParams);
-		System.out.println("in thrift Server main() ==6==");
-		server.serve();
-		timestamp = new Timestamp(System.currentTimeMillis());
-		System.out.println("in thrift Server main() ==7=="+timestamp.toString());
+			System.out.println("in thrift Server main() ");
+			InetSocketAddress serverAddress = new InetSocketAddress("benchmark-server", 8080);
+			System.out.println("in thrift Server main() ==2==");
+			TNonblockingServerTransport serverSocket = new TNonblockingServerSocket(serverAddress);
+			System.out.println("in thrift Server main() ==2.5==");
+			TThreadedSelectorServer.Args serverParams = new TThreadedSelectorServer.Args(serverSocket);
+			System.out.println("in thrift Server main() ==3==");
+			serverParams.protocolFactory(new TBinaryProtocol.Factory());
+			Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+			System.out.println("in thrift Server main() ==4=="+timestamp.toString());
+			serverParams.processor(new UserService.Processor<UserService.Iface>(new UserServiceServerImpl()));
+			System.out.println("in thrift Server main() ==5==");
+			TServer server = new TThreadedSelectorServer(serverParams);
+			System.out.println("in thrift Server main() ==6==");
+			server.serve();
+			timestamp = new Timestamp(System.currentTimeMillis());
+			System.out.println("in thrift Server main() ==7=="+timestamp.toString());
+		}catch (TTransportException e){
+			System.out.println("Server Exception is "+e.getMessage());
+		}
 	}
 
 }

dc8511ea086ee42a57e430439e9edfaca1ec272e
Facwl <2864235156@qq.com>
2022-01-23 15:56:28 +0800

    add auto get internel IP on Linux

--- thrift-accounts/target/classes/SearchService.thrift
+++
@@ -1,7 +0,0 @@
-namespace java benchmark.rpc.com.freeb.thrift
- 
-service SearchService {
-  
-	list<i64> IdealResEfficiencyTest(1: i32 totalComputationLoad,2:i32 threadName),
-
-}
--- thrift-accounts/target/generated-sources/annotations/com/freeb/jmh_generated/AccountsServer_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.freeb.jmh_generated;
-public class AccountsServer_jmhType extends AccountsServer_jmhType_B3 {
-}
-
